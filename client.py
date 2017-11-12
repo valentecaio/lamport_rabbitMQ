d02dfdcd5f52b16a108c3c70e778d1eb911a6a31
caio valente <caio-valente@hotmail.com>
2017-11-12 15:29:25 +0100

    [lamport] filtering own messages and locking keyboard when waiting

--- client.py
+++ client.py
@@ -10,12 +10,23 @@ try:
 except:
     pprint = print
 
-EXCHANGE = 'logs'
-Q_REQUESTS = 'requests'
+''' constants '''
+EXCHANGE = 'broadcast'
 MSG_REQUEST = 'REQUEST'
+DEBUG_FLAG = '[DEBUG]'
 
-global requests
-global own_queue_name
+requests = []
+
+
+'''
+Requesting process
+
+    1- Pushing its request in its own queue (ordered by time stamps)
+    2- Sending a request to every node.
+    3- Waiting for replies from all other nodes.
+    4- If own request is at the head of its queue and all replies have been received, enter critical section.
+    5- Upon exiting the critical section, remove its request from the queue and send a release message to every process.
+'''
 
 
 def enter_critical_section(seconds):
@@ -26,37 +37,57 @@ def enter_critical_section(seconds):
     print('EXIT critical section')
 
 
-def send_broadcast(msg):
-    channel.basic_publish(exchange=EXCHANGE, routing_key='', body=msg)
-
-
 def send_request(request_time):
+    # 1- push request to own queue
     request = Request(request_time, own_queue_name)
-    send_broadcast(MSG_REQUEST)
+    requests.append(request)
+
+    # 2- broadcast request msg
+    msg = MSG_REQUEST + ' ' + str(request_time)
+    channel.basic_publish(exchange=EXCHANGE, routing_key=own_queue_name, body=msg)
+
+    print(DEBUG_FLAG, 'request sent')
 
 
 def receive_data():
-    global own_queue_name
-
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % (body,))
+        global waiting_responses
 
-    # start to listen to broadcasted msgs
+        # ignore own messages
+        if method.routing_key == own_queue_name:
+            print(DEBUG_FLAG, "ignoring own message")
+            return
+
+        print(DEBUG_FLAG, "received %r from %r" % (body, method.routing_key))
+
+        # after receiving all responses, stop waiting
+        # TODO: only set boolean as False after receiving ALL responses
+        waiting_responses = False
+
+    # start to listen to broadcast msgs
     channel.basic_consume(callback, queue=own_queue_name, no_ack=True)
     channel.start_consuming()
 
 
 # used by user interface thread
 def read_keyboard():
+    global waiting_responses
+
     print("Type requests to send: \t")
 
     while 1:
         try:
             user_input = input("> ")
+            # ignore messages when waiting responses
+            if waiting_responses:
+                print("Request ignored: waiting responses for last request")
+                continue
             request_time = int(user_input)
+            # send request and wait for responses
             send_request(request_time)
-        except:
-            print("Your message should be a integer")
+            waiting_responses = True
+        except ValueError:
+            print("Your request should be an integer")
             continue
 
 
@@ -77,6 +108,9 @@ def run_threads():
 
 if __name__ == '__main__':
     global own_queue_name
+    global waiting_responses
+
+    waiting_responses = False
 
     # start connection
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -89,6 +123,7 @@ if __name__ == '__main__':
     # exclusive tag is for deleting the queue when disconnecting
     result = channel.queue_declare(exclusive=True)
     own_queue_name = result.method.queue
+    print("own queue name = %s", own_queue_name)
 
     # bind own queue to exchange
     channel.queue_bind(exchange=EXCHANGE, queue=own_queue_name)
